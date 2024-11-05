7bce6bf2d8d2fae1f88ea2a0175a435c13e1e169
Coco <galcocco@gmail.com>
2024-11-05 16:25:30 +0800

    Create a Next.js Application

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+const home = () => (
+    <h1 className="text-4xl font-black text-center text-pink-600">Welcome to the world of Next.js</h1>
+  );
+
+export default home;
